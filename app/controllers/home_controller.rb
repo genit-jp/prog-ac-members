71068b2a5a823d2873921d914c33185b6a1009ef
ryohorie <develop@ryohorie.com>
2020-05-03 00:28:02 +0900

    トップに今日のみんなを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,15 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except:  []
   def index
+    @date = Date.current
     @profiles = Profile.joins(:user).where("is_active = ?", true)
     @articles = Article.where(:is_active => true).order(priority: "DESC")
+    @attendances = Attendance.where("cast(date as text) LIKE ?", @date)
+    @timelines = []
+    for att in @attendances do
+      if att.user.profile.present? then
+        @timelines.push ["#{att.user.profile.name}#{att.is_remote ? '(リ)' : ''}", att.starts_at, att.ends_at]
+      end
+    end
   end
 end
