ab9307c05a4be0b6fd259ce09ffe4bb3012ec4f5
ryohorie <develop@ryohorie.com>
2020-05-02 23:20:10 +0900

    ホーム画面のメンバー一覧を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, except:  []
   def index
+    @profiles = Profile.joins(:user).where("is_active = ?", true)
     @articles = Article.where(:is_active => true).order(priority: "DESC")
   end
 end
