83e6d6b9e03d379af090c9e8ff7c2223c27cf1bc
ryohorie <develop@ryohorie.com>
2020-06-02 07:54:51 +0900

    今日の積み上げに変更

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -2,6 +2,34 @@ class TwitterController < ApplicationController
   protect_from_forgery except: [:fav]
 
   def fav_prog_ac
+    # client = Twitter::REST::Client.new do |config|
+    #   config.consumer_key = Rails.application.credentials.twitter_prog_ac[:consumer_key]
+    #   config.consumer_secret = Rails.application.credentials.twitter_prog_ac[:consumer_secret]
+    #   config.access_token = Rails.application.credentials.twitter_prog_ac[:access_token]
+    #   config.access_token_secret = Rails.application.credentials.twitter_prog_ac[:access_token_secret]
+    # end
+    #
+    # keywords = ['#プログラミング初心者']
+    # result = []
+    # for keyword in keywords do
+    #   tweets = client.search(keyword, {:lang => 'ja', :count => 100/keywords.length, :result_type => 'recent' })
+    #   result.push tweets.to_h[:statuses]
+    # end
+    # # すべてのツイートをcreated_atでソートする
+    # result.flatten!
+    # result.sort!{|a, b| b[:created_at] <=> a[:created_at] }
+    # ids = result.map{|tweet| tweet[:id] }
+    # ids.uniq!
+    # client.favorite(ids.take(40))
+    #
+    # render json: ids.to_json, :status => 200
+
+    hour = Time.current.hour
+    logger.debug "#{hour}時"
+    if hour < 6 or hour > 13 then
+      render json: {message: 'cancel'}, :status => 200
+      return
+    end
     client = Twitter::REST::Client.new do |config|
       config.consumer_key = Rails.application.credentials.twitter_prog_ac[:consumer_key]
       config.consumer_secret = Rails.application.credentials.twitter_prog_ac[:consumer_secret]
@@ -9,10 +37,10 @@ class TwitterController < ApplicationController
       config.access_token_secret = Rails.application.credentials.twitter_prog_ac[:access_token_secret]
     end
 
-    keywords = ['#プログラミング初心者']
+    keywords = ['#今日の積み上げ ']
     result = []
     for keyword in keywords do
-      tweets = client.search(keyword, {:lang => 'ja', :count => 100/keywords.length, :result_type => 'recent' })
+      tweets = client.search(keyword, {:lang => 'ja', :count => 50/keywords.length, :result_type => 'recent' })
       result.push tweets.to_h[:statuses]
     end
     # すべてのツイートをcreated_atでソートする
@@ -20,9 +48,10 @@ class TwitterController < ApplicationController
     result.sort!{|a, b| b[:created_at] <=> a[:created_at] }
     ids = result.map{|tweet| tweet[:id] }
     ids.uniq!
-    client.favorite(ids.take(40))
+    client.favorite(ids.take(20))
 
     render json: ids.to_json, :status => 200
+
   end
 
   def fav_genit
