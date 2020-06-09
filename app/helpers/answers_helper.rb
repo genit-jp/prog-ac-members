c9299a1182b5836a79978b8eaff5a74fdcb38652
ryohorie <develop@ryohorie.com>
2020-06-09 19:00:51 +0900

    不具合修正

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -5,6 +5,10 @@ module AnswersHelper
     while loop
       ret = ret + 1
       qs = questions.select{|q| q.level == ret and q.availabled}
+      if qs.length == 0 then
+        loop = false
+        break
+      end
       for q in qs do
         as = answers.select{|a| a.question_id == q.id}
         if as.empty? then
@@ -22,6 +26,10 @@ module AnswersHelper
     while loop
       ret = ret + 1
       qs = questions.select{|q| q.level == ret and q.availabled }
+      if qs.length == 0 then
+        loop = false
+        break
+      end
       for q in qs do
         as = answers.select{|a|
           a.question_id == q.id and a.code_reviews.select{|c| c.lgtm }.length >= 2
@@ -37,10 +45,9 @@ module AnswersHelper
 
   def self.get_reviewers(question)
     users = []
-    levels = Level.includes(:user).all
+    levels = Level.includes(user:[:profile]).all
     for level in levels do
-      if level.stage > question.stage or
-          level.stage >= question.stage and level.level > question.level then
+      if (level.stage > question.stage) or (level.stage >= question.stage and level.level > question.level) then
         users.push level.user
       end
     end
